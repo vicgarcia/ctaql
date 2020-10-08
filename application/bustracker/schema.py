3c0506b0d7937155dfd5fdee9444fbd3d0845d0d
Vic Garcia <vic.garcia@outlook.com>
2020-10-07 22:47:04 -0400

    add vehicles from api as type, field of route

--- application/bustracker/schema.py
+++ application/bustracker/schema.py
@@ -1,15 +1,42 @@
 import graphene
-from graphene_django import DjangoObjectType
+from graphene_django import DjangoObjectType, DjangoListField
+import bustracker.client as cta
 from .models import Route, Pattern, Stop
 
 
-class ShallowRouteType(DjangoObjectType):
+class RoutesType(DjangoObjectType):
     class Meta:
         model = Route
         fields = ('number', 'name')
 
 
+class VehicleType(graphene.ObjectType):
+    number = graphene.String()
+    pattern = graphene.String()
+    destination = graphene.String()
+    latitude = graphene.Float()
+    longitude = graphene.Float()
+    heading = graphene.Int()
+
+    @classmethod
+    def from_api_data(cls, data):
+        ''' hydrate a VehicleType with data from the cta api '''
+        return cls(
+            number=data['vid'],
+            pattern=data['pid'],
+            destination=data['des'],
+            latitude=data['lat'],
+            longitude=data['lon'],
+            heading=data['hdg'],
+        )
+
+
 class RouteType(DjangoObjectType):
+    vehicles = graphene.List(VehicleType)
+
+    def resolve_vehicles(self, info):
+        return [VehicleType.from_api_data(x) for x in cta.get_vehicles(rt=self.number)]
+
     class Meta:
         model = Route
         fields = ('number', 'name', 'patterns')
@@ -28,11 +55,8 @@ class StopType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    routes = graphene.List(ShallowRouteType)
+    routes = DjangoListField(RoutesType)
     route = graphene.Field(RouteType, number=graphene.String(required=True))
-
-    def resolve_routes(root, info):
-        return Route.objects.all()
 
     def resolve_route(root, info, number):
         try:
