57a2b951ededc835191f741b6d88b82fa5703341
Vic Garcia <vic.garcia@outlook.com>
2020-12-22 19:01:45 -0500

    remove health check endpoint

--- application/health.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK
-
-
-class HealthCheckView(APIView):
-
-    def get(self, request, *args, **kwargs):
-        return Response(status=HTTP_200_OK)
--- application/urls.py
+++ application/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
-from health import HealthCheckView
 from bustracker.schema import schema
 
 
 urlpatterns = [
-
-    # health check endpoint
-    path('health/', HealthCheckView.as_view()),
 
     # graphql endpoint
     path ('graphql/', GraphQLView.as_view(graphiql=True, schema=schema))
