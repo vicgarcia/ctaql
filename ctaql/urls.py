c1e723b2b6691275a7c00a36b8792baa4e37192c
Vic Garcia <vic.garcia@outlook.com>
2021-11-03 20:01:51 -0400

    rename application directory

--- ctaql/urls.py
+++ ctaql/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from bustracker.schema import schema
 
-
 urlpatterns = [
 
     # graphql endpoint
-    path('', GraphQLView.as_view(graphiql=True, schema=schema))
+    path('', GraphQLView.as_view(graphiql=True, schema=schema)),
 
 ]
