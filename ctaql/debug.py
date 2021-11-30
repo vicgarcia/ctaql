f70a5a1bfed982ebf5adb754efc73464d972c308
Vic Garcia <vic.garcia@outlook.com>
2021-11-29 22:47:16 -0500

    remove unneeded __main__ blocks

--- ctaql/debug.py
+++ ctaql/debug.py
@@ -3,9 +3,7 @@ import sys
 import ptvsd
 from configurations.management import execute_from_command_line
 
-if __name__ == "__main__":
+if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+    ptvsd.enable_attach(address=('0.0.0.0', 3000), redirect_output=True)
 
-    if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-        ptvsd.enable_attach(address=('0.0.0.0', 3000), redirect_output=True)
-
-    execute_from_command_line(sys.argv)
+execute_from_command_line(sys.argv)
--- ctaql/manage.py
+++ ctaql/manage.py
@@ -1,5 +1,4 @@
 import sys
 from configurations.management import execute_from_command_line
 
-if __name__ == "__main__":
-    execute_from_command_line(sys.argv)
+execute_from_command_line(sys.argv)
